3985a924e7a7850a1a0a6576605748853527dcf4
kylewstewart <kyle.w.stewart@mac.com>
2017-06-17 08:25:35 -0400

    cleaned up app.js and added comments

--- src/adapters/reviewsAdaptor.js
+++ src/adapters/reviewsAdaptor.js
@@ -6,39 +6,4 @@ export class ReviewsAdaptor {
     .then( res => res.json() )
   }
 
-  static create(name, location, image){
-    return fetch('http://localhost:3000/api/v1/reviews', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-      },
-      body: JSON.stringify({
-        restaurant: {
-          name: name,
-          location: location,
-          image: image
-        }
-      })
-    }).then(response => response.json() )
-  }
-
-
-  static update(restaurant){
-    return fetch(`http://localhost:3000/api/v1/restaurants/${restaurant.id}`, {
-      method: 'PATCH',
-      headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-      },
-      body: JSON.stringify({
-        restaurant : {
-          name : restaurant.name,
-          location: restaurant.location,
-          image: restaurant.image
-        }
-      })
-    })
-  }
-
 }
--- src/containers/app.js
+++ src/containers/app.js
@@ -13,36 +13,41 @@ export default class App extends Component {
     this.state = {
       places: [],
       random_places: [],
-    }
+      filters: {
+        cost: "free",
+        bodily_impact: "Yikes!",
+        recommended_for: "hang over city"}
+      }
   }
 
-
   componentDidMount(){
-    const filters = {
-    cost: "",
-    bodily_impact: "Yikes!",
-    recommended_for: "hang over city"}
+    this.getPlaces()
+    this.getRandomPlaces(this.state.filters)
+  }
 
-    this.getRandomPlaces(filters)
-
-    PlacesAdaptor.all().then(res => res.forEach(place => {
-      const reviews = this.getPlaceReviews(place.id)
-      this.setState(prevState => {
-        places: prevState.places.push({
-          id: place.id,
-          name: place.name,
-          location: place.location,
-          image: place.image,
-          reviews: reviews
+  // Calls API: places#all, places response into a Place object with an empty
+  // Reviews array and pushes each object into a place array in state
+  getPlaces(){
+    PlacesAdaptor.all()
+      .then(res => res.forEach(place => {
+        const reviews = this.getPlaceReviews(place.id)
+        this.setState(prevState => {
+          places: prevState.places.push({
+            id: place.id,
+            name: place.name,
+            location: place.location,
+            image: place.image,
+            reviews: reviews
           })
         })
       }))
-    }
+  }
 
-
+  // Calls API: reviews#all to recieves all reviews for a given Place, recieves
+  // an array of Reviews and replaces the Reviews array in that Place object
   getPlaceReviews(id){
     ReviewsAdaptor.all(id)
-    .then(res => { this.setState(prevState => {
+      .then(res => { this.setState(prevState => {
         return { places: prevState.places.map( place => {
             if (place.id !== id) {
               return place
@@ -51,19 +56,26 @@ export default class App extends Component {
               place.reviews = res
               return place
             }
-          })
-        }
-      })
-    })
+        })}
+      })})
   }
 
+  // Calls API: places#create and posts an object of filter settings, recieves
+  // a random ordered array of Place objects that also includes aggregate review
+  // statistics for each place and set state for random_places state.
   getRandomPlaces(filters){
-    PlacesAdaptor.create(filters).then(res => console.log(res)).then(res => this.setState({random_places: res}))
+    PlacesAdaptor.create(filters)
+    .then(res => console.log(res))
+      .then(res => this.setState({random_places: res}))
   }
 
   render() {
+    if(!this.state.places[0]) {
+      return <h3> Loading Page ...</h3>
+    } else {
       return (
         <div>
+
           <PlacesList places={this.state.places}/>
           <Route exact path='/places/:id'
             render={(routerProps) => {
@@ -75,6 +87,7 @@ export default class App extends Component {
             />
         </div>
       )
+    }
   }
 
 
